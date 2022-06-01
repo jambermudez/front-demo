ca0f251bf5a4d1b1847b1a32120af7dd17899611
jamartin <josemartinbermudez@gmail.com>
2022-06-01 18:53:37 +0200

    develop | Main page layout

--- front-demo/src/app/app.component.ts
+++ front-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, NgControl } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-demo';
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = '';
+
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
+
+  public accept(): void {
+    console.log('asdf');
+  }
 }
--- front-demo/src/app/app.module.ts
+++ front-demo/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule, MatInput } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  exports: [
+    MatInput
   ],
   providers: [],
   bootstrap: [AppComponent]
